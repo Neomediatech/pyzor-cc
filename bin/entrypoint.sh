900a6c2cbbf4196b6b06d9cdec1ef50abc3fcd64
Dario B <feddar@users.noreply.github.com>
2019-12-23 13:04:38 +0100

    Update entrypoint.sh

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
 LISTEN_PORT=${LISTEN_PORT:-}
+PYZOR_SERVER=${PYZOR_SERVER:-}
+
+if [ "${PYZOR_SERVER}" != "" ]; then
+    mkdir -p /root/.pyzor
+    echo "${PYZOR_SERVER}:24441" > /root/.pyzor/servers
+fi
 
 exec /tini -e 143 -- python3 ./pyzorsocket.py 0.0.0.0 ${LISTEN_PORT}
